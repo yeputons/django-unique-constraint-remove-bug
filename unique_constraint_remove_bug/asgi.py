ac16072e82cf11e576b10e4845e81daf4a5b8c38
Egor Suvorov <egor.suvorov@gmail.com>
2021-02-27 14:45:37 +0300

    Add initial project

---
+++ unique_constraint_remove_bug/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unique_constraint_remove_bug project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unique_constraint_remove_bug.settings')
+
+application = get_asgi_application()
